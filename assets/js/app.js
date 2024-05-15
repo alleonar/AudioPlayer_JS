180bcc351a60cf21a47f0e72cdf09492b809b8a8
alleonar <156660558+alleonar@users.noreply.github.com>
2024-05-15 15:48:46 +0200

    media queries done / add animation on song switch change cursor on playlist

--- assets/js/app.js
+++ assets/js/app.js
@@ -84,10 +84,14 @@ window.onload = async () => {
         //remise à zéro de la barre de progression(trackProgress)
         myAudioPlayer.currentTime = 0;
 
+        console.log(document.getElementById('trackImg').src)
+
         //affichage des données du morceau chargé
-        document.getElementById('trackImg').src = `./img/${playlist[trackNumber].cover}`;
-        document.getElementById('trackTitle').innerHTML = `${playlist[trackNumber].title}`;
-        document.getElementById('trackArtist').innerHTML = `${playlist[trackNumber].artist}`;
+        if (document.getElementById('trackImg').src === 'http://127.0.0.1:5500/') {
+            document.getElementById('trackImg').src = `./img/${playlist[trackNumber].cover}`;
+            document.getElementById('trackTitle').innerHTML = `${playlist[trackNumber].title}`;
+            document.getElementById('trackArtist').innerHTML = `${playlist[trackNumber].artist}`;
+        }
 
         //remise à zéro de l'affichage du tableau contenant la playlist (tableList)
         document.getElementById('tableList').innerHTML = "";
@@ -147,7 +151,6 @@ window.onload = async () => {
                     wave01.classList.remove('closingWave01');
                     wave02.classList.remove('closingWave02');
                 }
-
             })
         })
     }
@@ -155,8 +158,33 @@ window.onload = async () => {
     //fonction de loadingtrack (trackNumber correspond à l'index de lecture)
     async function loadTrack(trackNumber) {
 
-        //réutilisation de la fonction d'initialisation de lecture de piste
         await loadFirstTrack(trackNumber);
+
+        // TEST ANIMATION FLIP
+
+        // await loadFirstTrack(trackNumber);
+
+        document.getElementById('trackInfo').animate([
+            { transform: 'rotateY(0deg)' },
+            { transform: 'rotateY(90deg)' }
+        ], {
+            duration: 500
+        })
+
+        setTimeout(() => {
+            document.getElementById('trackImg').src = `./img/${playlist[trackNumber].cover}`;
+            document.getElementById('trackTitle').innerHTML = `${playlist[trackNumber].title}`;
+            document.getElementById('trackArtist').innerHTML = `${playlist[trackNumber].artist}`;
+            document.getElementById('trackInfo').animate([
+                { transform: 'rotateY(90deg)' },
+                { transform: 'rotateY(0deg)' }
+            ], {
+                duration: 500
+            }
+            )
+        },
+            500
+        )
 
         //mise en lecture automatique
         myAudioPlayer.play();
@@ -410,43 +438,46 @@ window.onload = async () => {
     // suivit du currentTime de l'élément audio et mise à jour des infos temporelles
     myAudioPlayer.addEventListener('timeupdate', (event) => {
 
-        //récupération et affichage du temps total de l'élément audio sous le format minutes secondes
-        let minutes = Math.floor(myAudioPlayer.duration / 60);
-        let seconds = Math.floor(myAudioPlayer.duration % 60);
+        if (!isNaN(myAudioPlayer.duration)) {
 
-        //vérification du temps total l'élément audio chargé
-        if (playingTrackDuration === 0 || myAudioPlayer.duration !== playingTrackDuration) {
+            //récupération et affichage du temps total de l'élément audio sous le format minutes secondes
+            let minutes = Math.floor(myAudioPlayer.duration / 60);
+            let seconds = Math.floor(myAudioPlayer.duration % 60);
 
-            //affichage du temps total du morceau(trackDuration)
-            if (seconds < 10) {
-                trackDuration.innerHTML = `${minutes}: 0${seconds}`;
-            } else {
-                trackDuration.innerHTML = `${minutes}: ${seconds}`;
+            //vérification du temps total l'élément audio chargé
+            if (playingTrackDuration === 0 || myAudioPlayer.duration !== playingTrackDuration) {
+
+                //affichage du temps total du morceau(trackDuration)
+                if (seconds < 10) {
+                    trackDuration.innerHTML = `${minutes}: 0${seconds}`;
+                } else {
+                    trackDuration.innerHTML = `${minutes}: ${seconds}`;
+                }
             }
+
+            // récupération du currentTime de l'élément audio chargé
+            let currentTime = myAudioPlayer.currentTime;
+
+            // affichage du temps écoulé du morceau(trackCurrentTime)
+            if ((myAudioPlayer.currentTime % 60) < 10) {
+                trackCurrentTime.innerHTML = `${Math.floor(currentTime / 60)}: 0${Math.floor(currentTime % 60)}`;
+            } else {
+                trackCurrentTime.innerHTML = `${Math.floor(currentTime / 60)}: ${Math.floor(currentTime % 60)}`;
+            }
+
+            // récupération du temps restant de l'élément audio chargé
+            let timeRemaining = Math.floor(myAudioPlayer.duration - myAudioPlayer.currentTime)
+
+            // affichage du temps restant du morceau(timeRemaining)
+            if ((timeRemaining % 60) < 10) {
+                trackTimeLeft.innerHTML = `${Math.floor(timeRemaining / 60)}: 0${Math.floor(timeRemaining % 60)}`;
+            } else {
+                trackTimeLeft.innerHTML = `${Math.floor(timeRemaining / 60)}: ${Math.floor(timeRemaining % 60)}`;
+            }
+
+            // mise à jour de la progress bar
+            trackProgress.value = Math.floor((myAudioPlayer.currentTime / myAudioPlayer.duration) * 100)
         }
-
-        // récupération du currentTime de l'élément audio chargé
-        let currentTime = myAudioPlayer.currentTime;
-
-        // affichage du temps écoulé du morceau(trackCurrentTime)
-        if ((myAudioPlayer.currentTime % 60) < 10) {
-            trackCurrentTime.innerHTML = `${Math.floor(currentTime / 60)}: 0${Math.floor(currentTime % 60)}`;
-        } else {
-            trackCurrentTime.innerHTML = `${Math.floor(currentTime / 60)}: ${Math.floor(currentTime % 60)}`;
-        }
-
-        // récupération du temps restant de l'élément audio chargé
-        let timeRemaining = Math.floor(myAudioPlayer.duration - myAudioPlayer.currentTime)
-
-        // affichage du temps restant du morceau(timeRemaining)
-        if ((timeRemaining % 60) < 10) {
-            trackTimeLeft.innerHTML = `${Math.floor(timeRemaining / 60)}: 0${Math.floor(timeRemaining % 60)}`;
-        } else {
-            trackTimeLeft.innerHTML = `${Math.floor(timeRemaining / 60)}: ${Math.floor(timeRemaining % 60)}`;
-        }
-
-        // mise à jour de la progress bar
-        trackProgress.value = Math.floor((myAudioPlayer.currentTime / myAudioPlayer.duration) * 100)
 
     });
 
